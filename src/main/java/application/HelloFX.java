fce2962879f04ee218fbee615b73893b81b87813
Jaller698 <c.soetorp@gmail.com>
2023-03-27 15:19:15 +0200

    test

--- src/main/java/application/HelloFX.java
+++ src/main/java/application/HelloFX.java
@@ -1,11 +1,6 @@
 package application;
 
-
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 
 public class HelloFX extends Application {
 
