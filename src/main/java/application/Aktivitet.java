2b33f18ac658278c8d5b88fbcc7c6067e6e7b675
GaySupremacy <nata847s@gmail.com>
2023-03-27 17:06:44 +0200

    Added classes

---
+++ src/main/java/application/Aktivitet.java
@@ -0,0 +1,9 @@
+package application;
+
+import java.util.ArrayList;
+
+public class Aktivitet {
+	public Projekt p;
+	public int[] tidBrugt;
+	public ArrayList<Medarbejder> medarbejdere = new ArrayList<Medarbejder>();
+}
